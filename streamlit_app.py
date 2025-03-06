1d35c0ee3a45148457cba4843eb9086b7aade1b9
Muhammad03jon <163680576+Muhammad03jon@users.noreply.github.com>
2025-03-07 00:39:32 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,17 @@ from sklearn.metrics import classification_report, confusion_matrix, roc_curve, 
 
 
 st.title('💵 Предсказание идеального тарифа для клиента')
+
+file_path = "https://raw.githubusercontent.com/Muhammad03jon/M_Olimov_Project/refs/heads/master/data.csv
+df = pd.read_csv(file_path, sep=",", header=None)
+
+with st.expander('Исходные данные'):
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("X (Признаки)")
+        X_raw = df.drop('ideal_plan', axis=1)
+        st.dataframe(X_raw)
+    with col2:
+        st.subheader("y (Целевая переменная)")
+        y_raw = df['ideal_plan']
+        st.dataframe(y_raw.to_frame())
