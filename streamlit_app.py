1e6c5ecca67ef665dc2e803099276b69cdc58805
Muhammad03jon <163680576+Muhammad03jon@users.noreply.github.com>
2025-03-07 00:22:34 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from catboost import CatBoostClassifier
+from sklearn.metrics import classification_report, confusion_matrix, roc_curve, auc
 
-st.title('🎈 App Name')
 
-st.write('Hello world!')
+st.title('💵 Предсказание идеального тарифа для клиента')
